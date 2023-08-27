f241c26ad493568389a519211743bd7df31521f3
Odunayo Ogungbure <odunayo@seamlesshr.com>
2023-08-27 21:05:17 +0100

    Implement login functionality

--- app/Http/Controllers/Auth/User/LoginController.php
+++ app/Http/Controllers/Auth/User/LoginController.php
@@ -6,11 +6,21 @@ use App\Helpers\RespondWith;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\User\LoginRequest;
 use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class LoginController extends Controller
 {
     public function __invoke(LoginRequest $request): JsonResponse
     {
-        return RespondWith::success();
+        $credentials = array_merge(
+            $request->only(['email', 'password']),
+            ['is_admin' => false]
+        );
+
+        if (!$token = auth()->attempt($credentials)) {
+            return RespondWith::error('Failed to authenticate user', code: Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        return RespondWith::success(['token'=> $token->toString()]);
     }
 }
